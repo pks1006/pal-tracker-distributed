7f778058f3269921c8b800797a20ca63246ec082
Piyush Shrivastava <piyush.cpp@gmail.com>
2019-03-08 11:37:39 -0500

    createdb and gradlew modified

--- databases/create_databases.sql
+++ databases/create_databases.sql
@@ -1,4 +1,3 @@
-/*
 DROP DATABASE IF EXISTS tracker_allocations_dev;
 DROP DATABASE IF EXISTS tracker_backlog_dev;
 DROP DATABASE IF EXISTS tracker_registration_dev;
@@ -7,12 +6,9 @@ DROP DATABASE IF EXISTS tracker_allocations_test;
 DROP DATABASE IF EXISTS tracker_backlog_test;
 DROP DATABASE IF EXISTS tracker_registration_test;
 DROP DATABASE IF EXISTS tracker_timesheets_test;
-*/
 
-CREATE USER  'tracker'@'localhost'
+CREATE USER IF NOT EXISTS 'tracker'@'localhost'
   identified by '';
-
-
 GRANT ALL PRIVILEGES ON *.* TO 'tracker' @'localhost';
 
 CREATE DATABASE tracker_allocations_dev;
